94f535440196ae8a67d60932b12bf991630da034
Bogdan Lytvyn <boddofish@gmail.com>
2021-02-11 20:47:27 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 import fetchMovies from "../src/js/fetchMovies.js";
 import "./styles.css";
 
 fetchMovies();
+=======
+import fetchMovies from '../src/js/fetchMovies.js';
+import './styles.css';
+
+fetchMovies();
+
+
+          
+>>>>>>> Stashed changes
