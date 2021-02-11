6ee8cae2ba5d4b3b725155a3f0b7508a3c6433eb
Ivan Khailo <jakom333@gmail.com>
2021-02-11 21:45:12 +0200

    madal window add

--- src/js/_modalWinMovie.js
+++ src/js/_modalWinMovie.js
@@ -1,9 +1,7 @@
-import "styles.css";
-
-const apiKey = "a3f1bffb5eca2c1e671aa27884e4851b";
+const KEY = "a3f1bffb5eca2c1e671aa27884e4851b";
 
 function fetchOneMovieInfo(movie_id) {
-  return fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${key}`)
+  return fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${KEY}`)
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
