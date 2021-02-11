9f6dfad3d84c30f6213e9edbca6cc5add9e3c515
Ivan Khailo <jakom333@gmail.com>
2021-02-11 20:58:17 +0200

    first update attemp

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-<<<<<<< Updated upstream
 import fetchMovies from "../src/js/fetchMovies.js";
 import "./styles.css";
 
 fetchMovies();
-=======
-import fetchMovies from '../src/js/fetchMovies.js';
-import './styles.css';
-
-fetchMovies();
-
-
-          
->>>>>>> Stashed changes
