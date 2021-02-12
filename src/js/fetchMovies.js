089acaea9d3b2896a4077fab0860e11f563f6180
Daria Viter <viter.daria@gmail.com>
2021-02-12 12:23:05 +0000

    commit changes

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,11 +1,17 @@
+<<<<<<< Updated upstream
 import cardTemplate from "../templates/movie-card.hbs";
 // import markup from './markup.js';
 
 //https://api.themoviedb.org/3/movie/550?api_key=6f1c32f58bd439b838f8f392fdf2c4dc
+=======
+import markup from './markup.js';
+
+>>>>>>> Stashed changes
 
 const KEY = "6f1c32f58bd439b838f8f392fdf2c4dc";
 
 
+<<<<<<< Updated upstream
 // fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${KEY}`)
 //   .then(response => (response.json())
 //     .then(data => {
@@ -14,6 +20,8 @@ const KEY = "6f1c32f58bd439b838f8f392fdf2c4dc";
 //     .catch(err => console.log(err)));
 
 const gallery = document.querySelector(".photo-gallery-list");
+=======
+>>>>>>> Stashed changes
 
 
 
@@ -30,6 +38,18 @@ export default function fetchMovies() {
     .catch((err) => console.log(err));
 }
 
+<<<<<<< Updated upstream
 function markup(data) {    
   gallery.innerHTML = data.results.map((cover) => cardTemplate(cover)).join('');
+=======
+
+export function fetchGenres() {
+  const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${KEY}&language=en-US`;
+  return fetch(genreUrl)
+    .then((response) => (response.status === 200 ? response.json() : ""))
+    .then((data) => {
+      return data.genres;
+    })
+    .catch((err) => console.log(err));
+>>>>>>> Stashed changes
 }
