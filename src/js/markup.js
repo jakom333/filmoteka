6253199258693586552170421907a68dac27ad3f
Daria Viter <viter.daria@gmail.com>
2021-02-13 13:05:14 +0000

    откорректировала стили

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,28 +1,43 @@
 import { genres } from "../index.js";
 import cardTemplate from "../templates/movie-card.hbs";
 
-
 const gallery = document.querySelector(".photo-gallery-list");
 
 export default function markup(data) {
-    
   gallery.innerHTML = data.results
-      .map((movie) => {
-        
-        let movieGenres = [];
-        
-          movie.title = movie.title.toUpperCase();
-        //   console.log(movie.title.length)
+    .map((movie) => {    
+      
+      movie.title = movie.title.toUpperCase();   
+      
+      if (movie.title.length > 20) {
+        movie.title = movie.title.substring(0,32) + '...';       
+      }
+     
+      let movieGenres = [];
       movie.genre_ids.forEach((el) => {
         const foundGenreName = genres.find((item) => item.id === el);
         movieGenres.push(" " + foundGenreName.name);
       });
-      movie.genre_ids = movieGenres.slice(0, 2);
+
+      !movieGenres.length ? movieGenres.push('Other') : '';
+
+      movie.genre_ids = movieGenres.slice(0, 2);     
 
       movie.release_date = movie.release_date.substring(0, 4);
-    //   console.log(movie);
+    
+      if (movie.poster_path)
+        movie.poster_path =
+          "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" +
+          movie.poster_path;
+      else
+        movie.poster_path =
+          "https://cdn.bookauthority.org/dist/images/book-cover-not-available.6b5a104fa66be4eec4fd16aebd34fe04.png";
+
       return cardTemplate(movie);
     })
     .join("");
 }
+
+
+
 
