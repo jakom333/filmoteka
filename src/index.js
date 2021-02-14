bda604f39e8521c71aa8be3f95319db91596ed9e
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2021-02-14 18:59:27 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,84 +1,11 @@
 import "./styles.css";
-import fetchMovies from "../src/js/fetchMovies.js";
 import "./js/header/header.js";
+import fetchMovies from "../src/js/fetchMovies.js";
 import { fetchGenres } from "./js/fetchMovies.js";
 import "./js/pagination.js";
 import "./js/spinner.js";
 import "./js/search-input.js";
-
-/* * ------------- modal-window ---------------- */
-import modalWindowTpl from "./templates/modal-window.hbs";
-
-const filmsList = document.querySelector(".photo-gallery-list");
-let film_ID;
-
-const modalWindow = document.querySelector(".modal");
-const modalContent = document.querySelector(".modal-content");
-const overlay = document.querySelector(".modal-overlay");
-const closeModalBtn = document.querySelector(
-  'button[data-action="close-modal"]',
-);
-
-filmsList.addEventListener("click", onOpenModal);
-overlay.addEventListener("click", onOverlayClick);
-closeModalBtn.addEventListener("click", onCloseModal);
-
-function onOpenModal(event) {
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-
-  const filmRef = event.target;
-  film_ID = filmRef.dataset.id;
-
-  const KEY = "8c70e92845ff03879b2dd3fe0ba57aa8";
-  let movieIUrl = `https://api.themoviedb.org/3/movie/${film_ID}?api_key=${KEY}&language=en-US`;
-
-  function fetchFilmInfo() {
-    return fetch(movieIUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        return modalMarkup(data);
-      })
-      .catch((err) => console.log(err));
-  }
-  fetchFilmInfo(film_ID);
-}
-
-function modalMarkup(data) {
-  modalWindow.classList.remove("is-hidden");
-
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-
-  const markup = modalWindowTpl(data);
-  modalContent.insertAdjacentHTML("afterbegin", markup);
-
-  document.body.classList.add("modal-open");
-  window.addEventListener("keydown", onPressKey);
-}
-
-function onCloseModal() {
-  window.removeEventListener("keydown", onPressKey);
-  modalWindow.classList.add("is-hidden");
-  document.body.classList.remove("modal-open");
-  modalContent.innerHTML = "";
-}
-
-function onPressKey(event) {
-  if (event.code === "Escape") onCloseModal();
-}
-
-function onOverlayClick(event) {
-  if (event.target === event.currentTarget) {
-    onCloseModal();
-  }
-}
-
-/* * ------------- end of modal-window ---------------- */
+import "./js/modal-window.js";
 
 export let genres = [];
 
---
+++ src/js/modal-window.js
@@ -0,0 +1,70 @@
+import modalWindowTpl from "../templates/modal-window.hbs";
+
+const filmsList = document.querySelector(".photo-gallery-list");
+let film_ID;
+
+const modalWindow = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const overlay = document.querySelector(".modal-overlay");
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-modal"]',
+);
+
+filmsList.addEventListener("click", onOpenModal);
+overlay.addEventListener("click", onOverlayClick);
+closeModalBtn.addEventListener("click", onCloseModal);
+
+function onOpenModal(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const filmRef = event.target;
+  film_ID = filmRef.dataset.id;
+
+  const KEY = "8c70e92845ff03879b2dd3fe0ba57aa8";
+  let movieIUrl = `https://api.themoviedb.org/3/movie/${film_ID}?api_key=${KEY}&language=en-US`;
+
+  function fetchFilmInfo() {
+    return fetch(movieIUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        return modalMarkup(data);
+      })
+      .catch((err) => console.log(err));
+  }
+  fetchFilmInfo(film_ID);
+}
+
+function modalMarkup(data) {
+  modalWindow.classList.remove("is-hidden");
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+
+  const markup = modalWindowTpl(data);
+  modalContent.insertAdjacentHTML("afterbegin", markup);
+
+  document.body.classList.add("modal-open");
+  window.addEventListener("keydown", onPressKey);
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onPressKey);
+  modalWindow.classList.add("is-hidden");
+  document.body.classList.remove("modal-open");
+  modalContent.innerHTML = "";
+}
+
+function onPressKey(event) {
+  if (event.code === "Escape") onCloseModal();
+}
+
+function onOverlayClick(event) {
+  if (event.target === event.currentTarget) {
+    onCloseModal();
+  }
+}
