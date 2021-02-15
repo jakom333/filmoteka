926c72d5096d14366c1b527a4d54fe9329ebd1ab
Bogdan Lytvyn <boddofish@gmail.com>
2021-02-15 03:11:03 +0200

    trailer done

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -3,42 +3,62 @@ const key = "4fbdbd8abdbcde78896e194e86813212";
 const baseUrl = "https://api.themoviedb.org/3";
 
 function fetchUrl(movieID) {
-  console.log(movieID);
   const url = `${baseUrl}/movie/${movieID}/videos?api_key=${key}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       let youtubeKey = data.results[0].key;
-      console.log(youtubeKey);
       backTrailerRef.classList.add("is-open");
       backTrailerRef.insertAdjacentHTML(
         "beforeend",
-        `<iframe class ="trailer"
-          src="http://www.youtube.com/embed/${youtubeKey}?autoplay=1&color=white&modestbranding=1" 
-          frameborder="0"
-           allow="accelerometer; picture-in-picture; autoplay"
-          allowfullscreen
-          ></iframe>
-          `,
+        `<div class="trailer-iframe">
+      <iframe class ="trailer"
+      type="text/html"
+      src="http://www.youtube.com/embed/${youtubeKey}?color=white&modestbranding=1" 
+      frameborder="0"
+      allow="accelerometer; picture-in-picture; autoplay"
+      allowfullscreen
+      ></iframe>
+      </div>
+      `,
       );
+      const trailerFrameRefs = document.querySelector(".trailer-iframe");
+      console.log(trailerFrameRefs);
+      setTimeout(() => {
+        trailerFrameRefs.insertAdjacentHTML(
+          "beforeend",
+          `<button class="btn-close-player">
+      <svg class="btn-close-icon">
+      <use href="./images/symbol-defs.svg#icon-cancel"></use>
+      </svg>
+      </button>`,
+        );
+      }, 500);
     })
     .catch(() => {
       backTrailerRef.classList.add("is-open");
       backTrailerRef.insertAdjacentHTML(
         "beforeend",
-        `<iframe class ="trailer"
-         src="http://www.youtube.com/embed/50?autoplay=1&color=white&modestbranding=1" 
+        `<div class="trailer-iframe">
+        <iframe class ="trailer"
+        type="text/html"
+          src="http://www.youtube.com/embed/551?color=white&modestbranding=1" 
           frameborder="0"
-           allow="accelerometer; picture-in-picture; autoplay"
+          allow="accelerometer; picture-in-picture; autoplay"
           allowfullscreen
           ></iframe>
+          <button class="btn-close-player">
+            <svg class="btn-close-icon">
+              <use href="./images/symbol-defs.svg#icon-cancel"></use>
+            </svg>
+          </button>
+          </div>
           `,
       );
     });
 }
 
 function escPlayerHandler(event) {
-  console.log(event.code);
   if (event.code === "Escape") {
     onClosePlayer();
   }
@@ -59,38 +79,8 @@ export function playTrailer() {
 
   function showPosterPlay(event) {
     let movieID = event.target.dataset.id;
-    console.log(movieID);
     fetchUrl(movieID);
   }
   posterSvgRefs.addEventListener("click", showPosterPlay);
   buttonTrailerRefs.addEventListener("click", showPosterPlay);
 }
-
-//  <button class="btn-close-player">
-//    <svg class="btn-close-icon">
-//      <use href="./images/symbol-defs.svg#icon-cancel"></use>
-//    </svg>
-//  </button>;
-// async fetchUrl() {
-//     const response = await fetch(this.url);
-//     const movie = await response.json();
-//     this.fetchUrl().then((data) => {
-//       let youtubeKey = data.results[0].key;
-//       console.log(youtubeKey);
-
-//       const traiLler = document.body.insertAdjacentHTML(
-//         "beforeend",
-//         `<iframe
-//       class ="trailer"
-//         width="320"
-//         height="160"
-//         src="http://www.youtube.com/embed/${youtubeKey}?enablejsapi=1&origin=http://example.com"
-//         frameborder="0"
-//         allow="accelerometer; autoplay=1; picture-in-picture; allowfullscreen"
-//         allowfullscreen
-//       ></iframe>
-//       <button type="text" class="trailer-close">x</button>
-//        `,
-//       );
-//     });
-//   },
