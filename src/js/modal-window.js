be484c84f75371daa75da8e6d4964f0c563cafdc
Daria Viter <viter.daria@gmail.com>
2021-02-15 20:50:37 +0000

    удалила лишние console.log

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -53,8 +53,7 @@ function modalMarkup(data) {
   const markup = modalWindowTpl(data);
   modalContent.insertAdjacentHTML("afterbegin", markup);
   window.addEventListener("keydown", onPressKey);
-  const watchBtn = document.querySelector(".action-watch");
-  console.log(watchBtn);
+  const watchBtn = document.querySelector(".action-watch");  
   
   const watchedInLocalstorage = JSON.parse(localStorage.getItem("watched"));
   if (checkFilm(watchedInLocalstorage, data)) {
