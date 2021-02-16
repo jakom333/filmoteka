61eed3e0bade661629dbf5a5390749e1f28d4a0d
Liudmila Popescu <liudmila.popescu91@gmail.com>
2021-02-16 17:40:04 +0000

    genres fetch problem- solved

--- src/js/switch-language.js
+++ src/js/switch-language.js
@@ -1,8 +1,6 @@
 import refs from './refs.js';
 import translatedData from '../data-base/translated.json';
 import fetchMovies, { fetchGenres } from "./fetchMovies.js";
-// import { genres } from '../index.js';
-
 
 
 export default
@@ -12,10 +10,8 @@ function switchLangHandler(event) {
   event.preventDefault();
   let lang;
 
-  if (event.target.classList.contains('underline')) {
-    event.target.classList
-  }
- let activeLang = refs.switcher.querySelector('.underline');
+
+let activeLang = refs.switcher.querySelector('.underline');
   activeLang.classList.remove('underline');
   event.target.classList.add('underline');
   
@@ -25,28 +21,16 @@ function switchLangHandler(event) {
     lang= 'RU'
   };
   
-   document.documentElement.lang = lang.toLocaleLowerCase();
+  document.documentElement.lang = lang.toLocaleLowerCase();
   changeLangSearch(lang);
 
-  let genres = [];
-  fetchGenres().then((res) => {
-  genres = res;
-  fetchMovies();
-});
-
-
-    // fetchGenres();
-    // fetchMovies();
   
-  refs.input[0].placeholder = translatedData[lang].input;
-  refs.homeBtn.textContent = translatedData[lang].home;
-  refs.byStudents.textContent = translatedData[lang].footerStudents;
-  refs.libraryBtn.textContent=translatedData[lang].library;
-  refs.watchedBtn.textContent=translatedData[lang].watchedButton;
-  refs.queueBtn.textContent=translatedData[lang].queueButton;
-  refs.copyright.textContent=translatedData[lang].copyright;
-  refs.developed.textContent=translatedData[lang].footerDeveloped;
-  refs.by.textContent = translatedData[lang].by;
+  fetchGenres(lang).then((res) => {
+  fetchMovies();
+  });
+
+  traslateHTMLtext(lang);
+  
 
   };
   
@@ -57,4 +41,16 @@ function switchLangHandler(event) {
       localStorage.setItem('lang', 'en-US');
     }
 };
+
+function traslateHTMLtext(lang){
+  refs.input[0].placeholder = translatedData[lang].input;
+  refs.homeBtn.textContent = translatedData[lang].home;
+  refs.byStudents.textContent = translatedData[lang].footerStudents;
+  refs.libraryBtn.textContent=translatedData[lang].library;
+  refs.watchedBtn.textContent=translatedData[lang].watchedButton;
+  refs.queueBtn.textContent=translatedData[lang].queueButton;
+  refs.copyright.textContent=translatedData[lang].copyright;
+  refs.developed.textContent=translatedData[lang].footerDeveloped;
+  refs.by.textContent = translatedData[lang].by;
+}
     
