e32a807f7b01d91095011491818b6e3c633c4991
Daria Viter <viter.daria@gmail.com>
2021-02-16 20:06:41 +0000

    удалила markupWatchedMovies
    удалила markupWatchedMovies,так как вместо нее используем теперь универсальную функцию markup

--- src/js/markup-library.js
+++ src/js/markup-library.js
@@ -1,5 +1,5 @@
-
-import libraryTemplate from "../templates/library-template.hbs";
+import { markup } from "./markup.js";
+import cardTemplate from "../templates/movie-card.hbs";
 import fetchMovies from "./fetchMovies.js";
 
 const gallery = document.querySelector(".photo-gallery-list");
@@ -41,34 +41,9 @@ function queueBtnHandler() {
 export function markupLibrary(data) {
   if (!data)
     return;
-  data.map((movie) => markupWatchedMovies(movie));  
-  gallery.innerHTML = libraryTemplate(data);
+  data.map((movie) => markup(movie));  
+  gallery.innerHTML = cardTemplate(data);
 }
-
-function markupWatchedMovies(movie) {  
-  movie.title = movie.title.toUpperCase();
-  
-  let movieGenres = [];
-  movie.genres.forEach((el) => {   
-      movieGenres.push(" " + el.name);
-  });
-  
-  !movieGenres.length ? movieGenres.push("Other") : "";
-  movie.genres = movieGenres.slice(0, 2);
-  
-  movie.release_date = movie.release_date.substring(0, 4);
-
-    if (movie.poster_path)
-      movie.poster_path =
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" +
-        movie.poster_path;
-    else
-      movie.poster_path =
-        "https://sales.arecontvision.com/images/products/img_placeholder_41845_xl.jpg";
-
-    return movie;
-}
-
 
 homeBtn.addEventListener('click', homeBtnHandler)
 
