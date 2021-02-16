7392c09365a59148e58d5a6736c8ad05ab7b8639
Daria Viter <viter.daria@gmail.com>
2021-02-16 20:04:47 +0000

    приведение к объекту одной структуры
    если был ключ movie.genres - перезаписываем,нет-удаляем

--- src/js/markup.js
+++ src/js/markup.js
@@ -8,10 +8,13 @@ export default function markupSearch(data) {
   data.results.map((movie) => markup(movie));
   gallery.innerHTML = cardTemplate(data.results);
   const li = document.querySelectorAll(".photo-gallery-item");
-  movieButtons(li);
+  movieButtons(li, data);
 }
 
 export function markup(movie) {
+  if (movie.wasMarkedUp)
+    return;
+
   movie.title = movie.title.toUpperCase();
 
   if (movie.title.length > 33) {
@@ -19,14 +22,21 @@ export function markup(movie) {
   }
 
   let movieGenres = [];
-  movie.genre_ids.forEach((el) => {
-    const foundGenreName = genres.find((item) => item.id === el);
-    movieGenres.push(" " + foundGenreName.name);
-  });
+  if (movie.genre_ids) {
+    movie.genre_ids.forEach((el) => {
+      const foundGenreName = genres.find((item) => item.id === el);
+      if (foundGenreName)
+        movieGenres.push(" " + foundGenreName.name);
+    });
+  }
+  else if (movie.genres) {
+    movie.genres.forEach((el) => {
+        movieGenres.push(" " + el.name);
+    });
+  }
 
   !movieGenres.length ? movieGenres.push("Other") : "";
-
-  movie.genre_ids = movieGenres.slice(0, 2);
+  movie.genres = movieGenres.slice(0, 2);
 
   movie.release_date = movie.release_date.substring(0, 4);
 
@@ -37,6 +47,8 @@ export function markup(movie) {
   else
     movie.poster_path =
       "https://sales.arecontvision.com/images/products/img_placeholder_41845_xl.jpg";
+  
+  movie.wasMarkedUp = true;
 
   return movie;
 }
