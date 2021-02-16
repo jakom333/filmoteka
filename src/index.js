91767563ea07fa6878dece9650aeaa04af04b695
Liudmila Popescu <liudmila.popescu91@gmail.com>
2021-02-16 17:38:23 +0000

    imported refs.js + function for lang search after page refresh

--- src/index.js
+++ src/index.js
@@ -12,10 +12,26 @@ import "./js/localstorage/localstorage.js";
 import "./js/markup-library.js";
 import "./js/markup.js";
 import "./js/buttons-movie";
+import refs from './js/refs.js';
 
-export let genres = [];
+export let genres =  [];
 
-fetchGenres().then((res) => {
+let lang = localStorage.getItem('lang');
+let activeLang;
+if (lang === 'en-EN') {
+  activeLang = refs.switcher.querySelector('.underline');
+  activeLang.classList.remove('underline');
+  refs.enBtn.classList.add('underline');
+
+}  else if (lang === 'ru-RU'){
+  activeLang = refs.switcher.querySelector('.underline');
+  activeLang.classList.remove('underline');
+  refs.ruBtn.classList.add('underline');
+}
+
+fetchGenres(lang).then((res) => {
   genres = res;
+  localStorage.setItem('genres', JSON.stringify(genres));
   fetchMovies();
 });
+
