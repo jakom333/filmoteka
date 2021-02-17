f264d388847a3c69dfa2f7c460a167ba54db1a41
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-02-17 15:52:17 +0200

    commit

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,14 +1,14 @@
 import markupSearch from "./markup.js";
-import { currentPage } from "./pagination";
-import './search-input.js';
-import renderTopRated from './top-filters.js';
-import config from '../data-base//config.json';
-
-export default function fetchMovies() {
+import { currentPage } from "./pagination.js";
+import "./search-input.js";
+import renderTopRated from "./top-filters.js";
+import config from "../data-base//config.json";
+import markupPagination from "./pagination.js";
 
 let totalPages;
 
-  const lang = localStorage.getItem('lang');
+export default function fetchMovies() {
+  const lang = localStorage.getItem("lang");
   const url = `${config.baseURL}trending/movie/day?api_key=${config.KEY}&page=${currentPage}&language=${lang}`;
 
   return fetch(url)
@@ -16,20 +16,21 @@ let totalPages;
     .then((data) => {
       renderTopRated(data);
       markupSearch(data);
-      document.querySelector(".btn-last").textContent = data.total_pages;
-      document.querySelector(".btn-last").dataset.index = data.total_pages;
+      markupPagination(data);
+      // document.querySelector(".btn-last").textContent = data.total_pages;
+      // document.querySelector(".btn-last").dataset.index = data.total_pages;
       return (totalPages = data.total_pages);
     })
     .catch((err) => console.log(err));
 }
 
 export function fetchGenres(lang) {
-const genreUrl = `${config.baseURL}genre/movie/list?api_key=${config.KEY}&language=${lang}`;
+  const genreUrl = `${config.baseURL}genre/movie/list?api_key=${config.KEY}&language=${lang}`;
   return fetch(genreUrl)
     .then((response) => (response.status === 200 ? response.json() : ""))
     .then((data) => {
-    localStorage.setItem('genres', JSON.stringify(data.genres));
-    return data.genres
+      localStorage.setItem("genres", JSON.stringify(data.genres));
+      return data.genres;
     })
     .catch((err) => console.log(err));
 }
