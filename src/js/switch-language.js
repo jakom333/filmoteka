14412df74f419a5ba097abc85c7c8b5c68812851
Liudmila Popescu <liudmila.popescu91@gmail.com>
2021-02-17 08:45:01 +0000

    fixed switcher bug

--- src/js/switch-language.js
+++ src/js/switch-language.js
@@ -3,32 +3,56 @@ import translatedData from "../data-base/translated.json";
 import fetchMovies, { fetchGenres } from "./fetchMovies.js";
 
 
-export default
+
+let lang = localStorage.getItem('lang');
+let activeLang;
+
+if (lang === 'en-EN') {
+  activeLang = refs.switcher.querySelector('.circle-color');
+  activeLang.classList.remove('circle-color');
+  refs.enBtn.classList.add('circle-color');
+  translateHTMLtext('EN');
+
+}  else if (lang === 'ru-RU'){
+  activeLang = refs.switcher.querySelector('.circle-color');
+  activeLang.classList.remove('circle-color');
+  refs.ruBtn.classList.add('circle-color');
+  translateHTMLtext('RU');
+}  
+  
   
 refs.switcher.addEventListener('click', switchLangHandler);
+
 function switchLangHandler(event) {
   event.preventDefault();
-  let lang;
 
+  if (event.target.nodeName !== 'A') {
+    return;
+  }
+  else {
+    if (event.target === refs.enBtn) {
+      lang = "EN";
+    } else if (event.target === refs.ruBtn) {
+      lang = "RU";
+    };
+  }
+   
   let activeLang = refs.switcher.querySelector(".circle-color");
   activeLang.classList.remove("circle-color");
   event.target.classList.add("circle-color");
-
-  if (event.target === refs.enBtn) {
-    lang = "EN";
-  } else if (event.target === refs.ruBtn) {
-    lang = "RU";
-  }
 
   document.documentElement.lang = lang.toLocaleLowerCase();
   changeLangSearch(lang);
   translateHTMLtext(lang);
 
+  
   fetchGenres(lang).then((res) => {
   fetchMovies();
   });
+  };
+  
 
- };
+
   
   function changeLangSearch(lang) {
     if (lang === 'RU') {   
@@ -36,9 +60,9 @@ function switchLangHandler(event) {
     } else {
       localStorage.setItem('lang', 'en-US');
     }
-};
+  };
 
-export function translateHTMLtext(lang) {
+ function translateHTMLtext(lang) {
   
   refs.input[0].placeholder = translatedData[lang].input;
   refs.homeBtn.textContent = translatedData[lang].home;
@@ -49,5 +73,5 @@ export function translateHTMLtext(lang) {
   refs.copyright.textContent = translatedData[lang].copyright;
   refs.developed.textContent = translatedData[lang].footerDeveloped;
   refs.by.textContent = translatedData[lang].by;
-}
+  };
     
