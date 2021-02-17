22086be209f1ac7952924b7d8ae438cd66f5245b
Liudmila Popescu <liudmila.popescu91@gmail.com>
2021-02-17 08:41:40 +0000

    deleted from index.js

--- src/index.js
+++ src/index.js
@@ -12,28 +12,11 @@ import "./js/localstorage/localstorage.js";
 import "./js/markup-library.js";
 import "./js/markup.js";
 import "./js/buttons-movie";
-import refs from './js/refs.js';
-import { translateHTMLtext } from './js/switch-language.js';
 
-
-
-let lang = localStorage.getItem('lang');
-let activeLang;
-
-if (lang === 'en-EN') {
-  activeLang = refs.switcher.querySelector('.circle-color');
-  activeLang.classList.remove('circle-color');
-  refs.enBtn.classList.add('circle-color');
-  translateHTMLtext('EN');
-
-}  else if (lang === 'ru-RU'){
-  activeLang = refs.switcher.querySelector('.circle-color');
-  activeLang.classList.remove('circle-color');
-  refs.ruBtn.classList.add('circle-color');
-  translateHTMLtext('RU');
-}
 
 export let genres =  [];
+
+let lang = localStorage.getItem('lang');
 
 fetchGenres(lang).then((res) => {
   genres = res;
@@ -41,17 +24,3 @@ fetchGenres(lang).then((res) => {
   fetchMovies();
 });
 
-
-
-// let localStorageLang = localStorage.getItem("lang");
-// let activeLang;
-
-// if (localStorageLang === "en-EN") {
-//   activeLang = refs.switcher.querySelector(".circle-color");
-//   activeLang.classList.remove("circle-color");
-//   refs.enBtn.classList.add("circle-color");
-// } else if (localStorageLang === "ru-RU") {
-//   activeLang = refs.switcher.querySelector(".circle-color");
-//   activeLang.classList.remove("circle-color");
-//   refs.ruBtn.classList.add("circle-color");
-// }
